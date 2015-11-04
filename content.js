b7cabd02c81adebf25143eada61d77745f790c9e
思維漫走 <silver_ras@163.com>
2015-11-04 16:22:01 +0800

    push

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
 console.info('JD Book Plugin Loaded.')
 
 
-function AppendDoubanLabel(){
+function send2douban(){
     var bookItemParams=document.querySelectorAll('#parameter2 > li')
     var ISBN=null
     if(bookItemParams!=null && bookItemParams.length>2){
@@ -20,30 +20,42 @@ function AppendDoubanLabel(){
             if(xhr.readyState == 4){
                 if(xhr.status==200){
                     var data=JSON.parse(xhr.responseText)
-
                     BOOK_ENTITY=data
-
-                    var label=document.createElement('div')
-                    label.className="dt"
-                    label.innerText="豆瓣评分："
-
-                    var rating=document.createElement('div')
-                    rating.className="dd"
-                    rating.innerText=data.rating.average + " 分 (" + (data.rating.numRaters > 10 ? data.rating.numRaters : "少于10") + "人评价)"
-
-                    var ret=document.createElement('div')
-                    ret.id="summary-douban"
-                    ret.appendChild(label)
-                    ret.appendChild(rating)
-
-                    var summary=document.getElementById('summary')
-                    summary.appendChild(ret)
+                    appendDoubanLabel(data)
+                } else if(xhr.status==404){
+                    appendDoubanLabel(null)
                 }
             }
         }
+
+        xhr.onerror=function(){
+            appendDoubanLabel(null)
+        }
+
         xhr.open("GET", 'https://api.douban.com/v2/book/isbn/'+ISBN, true);
         xhr.send();
     }
 }
 
-AppendDoubanLabel()
+function appendDoubanLabel(data){
+    var label=document.createElement('div')
+    label.className="dt"
+    label.innerText="豆瓣评分："
+
+    var rating=document.createElement('div')
+    rating.className="dd"
+    if(data!==null)
+        rating.innerText=data.rating.average + " 分 (" + (data.rating.numRaters > 10 ? data.rating.numRaters : "少于10") + "人评价)"
+    else
+        rating.innerText="抱歉，无法在豆瓣发现对应的书籍资料"
+
+    var ret=document.createElement('div')
+    ret.id="summary-douban"
+    ret.appendChild(label)
+    ret.appendChild(rating)
+
+    var summary=document.getElementById('summary')
+    summary.appendChild(ret)
+}
+
+send2douban()
