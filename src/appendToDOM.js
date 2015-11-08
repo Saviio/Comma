442b9d833ed9b92c3d60cff4575a5182fcf9df54
思維漫走 <silver_ras@163.com>
2015-11-08 19:55:36 +0800

    add support scope

--- src/appendToDOM.js
+++ src/appendToDOM.js
@@ -6,16 +6,15 @@ function appendDoubanLabel(data, type){
 
     var ct = data !== null ? "{{0}} 分 (" + (data.rating.numRaters >= 10 ? "{{1}}" : "少于10") + "人评价)" : "抱歉，无法在豆瓣发现对应的书籍资料"
 
-
     var template={
         jd:''
             +'<div id="summary-douban">'
             +'<div class="dt">豆瓣评分：</div>'
             +'<div class="dd">'+ct+'</div>'
             +'</div>',
-        amazon:'<div>豆瓣评分:'+ct+'</div>'
+        amazon:'<div>豆瓣评分:'+ct+'</div>',
+        dangdang:'<span>&nbsp;&nbsp;豆瓣评分:'+ct+'</span>'
     }
-
 
     var dom = undefined
     if(data !== null)
--- src/operation.js
+++ src/operation.js
@@ -20,6 +20,12 @@ var bookstore={
         re:/https?:\/\/.*\.jd\.com/,
         selector:'#parameter2 li',
         container:'#summary'
+    },
+    dangdang:{
+        name:'dangdang',
+        re:/https?:\/\/.*\.dangdang\.com/,
+        selector:'.pro_content > ul.key > li',
+        container:'.pinglun'
     }
 }
 
@@ -28,33 +34,30 @@ function init(ref, acc){
     acc=acc || 0
     var type=null
     for(var i in bookstore){
-        if(bookstore[i].re.test(window.location.href)){
+        if(bookstore[i].re.test(WIN.location.href)){
             type=bookstore[i]
             break
         }
     }
 
     if(type!==null){
-        var info = document.querySelectorAll(type.selector)
+        var info = DOM.querySelectorAll(type.selector)
         if(info == null && acc<3){
             setTimeout(init(ref, acc++), 1000)
         } else if(info.length >= 1) {
-
-            var ISBN = null
-            for(var i = 0;i < info.length; i++){
-                var ret=info[i].innerText.match(/ISBN[:|：]\W?(\d{10,})/)
-                if(ret && ret.length > 0)
-                    ISBN = ret[1]
-            }
-
-            if(ISBN != null){
-                connect('GET','https://api.douban.com/v2/book/isbn/'+ISBN)
-                .then(function(data){
-                    ref['data'] = data
-                    appendToDOM(data, type)
-                },function(e){
-                    appendToDOM(null, type)
-                })
+            for(var i = 0; i < info.length; i++){
+                var ret = info[i].innerText.match(/ISBN[:|：]\W?(\d{10,})/)
+                if(ret && ret.length > 0){
+                    var ISBN = ret[1]
+                    connect('GET','https://api.douban.com/v2/book/isbn/'+ISBN)
+                    .then(function(data){
+                        ref['data'] = data
+                        appendToDOM(data, type)
+                    },function(e){
+                        appendToDOM(null, type)
+                    })
+                    break
+                }
             }
         }
     }
@@ -65,7 +68,6 @@ function init(ref, acc){
 function showDetail(ref){
     if(ref.data===null || ref.data===undefined)
         return
-
     var iframe='<iframe src="{{0}} style="{{1}}"></iframe>"'
 }
 
